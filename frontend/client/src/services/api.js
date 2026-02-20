1da0f2bcd6b056b3f0787ad4b616a2e645e6f69c
Tharasri <tharasri_23it58@kgkite.ac.in>
2026-02-20 10:40:19 +0530

    api.js changed

--- frontend/client/src/services/api.js
+++ frontend/client/src/services/api.js
@@ -1,10 +1,12 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const sendContactMessage = async (data) => {
-  const res = await fetch("http://localhost:5000/api/contact", {
+  const res = await fetch(`${API_URL}/api/contact`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   });
 
   if (!res.ok) {
